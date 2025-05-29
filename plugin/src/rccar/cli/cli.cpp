23ac3960f26fdbbe95a4c318e87f6ee655fa1544
Musca Mihail <muscamihaill@gmail.com>
2025-05-29 16:13:08 +0300

    added stun client imgui, added cli stun client, testing weird multithreading bug

--- plugin/src/rccar/cli/cli.cpp
+++ plugin/src/rccar/cli/cli.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <memory>
+#include <thread>
 #include <telebot/utils/logging.h>
 #include <telebot/cli/cli.h>
 #include <telebot/utils/stun.h>
@@ -12,10 +13,32 @@ namespace rccar::cli {
 namespace log = telebot::utils::logging;
 namespace stun = telebot::utils::stun;
 
+static stun::Server* server;
+static stun::Client* client;
+
+struct ClientListener : public stun::ClientListener {
+    void onList(stun::Client* client, const std::vector<std::string>& list) {
+        for (const auto& item : list) {
+            log::info("{}", item);
+        }
+    }
+};
+
+static void stun_client(const std::string& ip, int port, const std::string& name) {
+    log::info("Starting STUN server on port {}", port);
+
+    client = new stun::Client(std::make_shared<ClientListener>(), name);
+    client->connect(ip, port);
+
+    while (client->isConnected()) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+}
+
 static void stun_server(int port) {
     log::info("Starting STUN server on port {}", port);
 
-    stun::Server* server = new stun::Server(std::make_shared<stun::ServerListener>());
+    server = new stun::Server(std::make_shared<stun::ServerListener>());
     server->start(port);
 
     while (server->isRunning()) {
@@ -27,7 +50,7 @@ int main_cli(const telebot::plugins::Plugin& plugin) {
     const std::vector<std::string>& args = telebot::cli::args;
 
     if (args.size() < 3 || args[2] == "--help") {
-        log::info("Usage: {} {} <--help|--stun-server>", args[0], args[1]);
+        log::info("Usage: {} {} <--help|--stun-server|--stun-client>", args[0], args[1]);
         return 1;
     }
 
@@ -54,6 +77,32 @@ int main_cli(const telebot::plugins::Plugin& plugin) {
         }
 
         stun_server(port);
+    } else if (args[2] == "--stun-client") {
+        if (args.size() < 6) {
+            log::info("Usage: {} {} --stun-client <ip> <port> <name>", args[0], args[1]);
+            return 1;
+        }
+
+        std::string ip = args[3];
+        int port = 0;
+        try {
+            port = std::stoi(args[4]);
+
+            if (port < 1 || port > 65535) {
+                log::info("Port number must be between 1 and 65535");
+                return 1;
+            }
+        } catch (const std::invalid_argument& e) {
+            log::info("Invalid port number: {}", args[4]);
+            return 1;
+        } catch (const std::out_of_range& e) {
+            log::info("Port number out of range: {}", args[4]);
+            return 1;
+        }
+
+        std::string name = args[5];
+        
+        stun_client(ip, port, name);
     } else {
         log::info("Unknown option: {}", args[2]);
         return 1;
--- plugin/src/rccar/rccar.cpp
+++ plugin/src/rccar/rccar.cpp
@@ -1,17 +1,34 @@
 #include "rccar/rccar.h"
 
+#include <memory>
+#include <thread>
+#include <imgui.h>
+#include <SDL3/SDL.h>
 #include <telebot/telebot.h>
 #include <telebot/events.h>
 #include <telebot/utils/logging.h>
 #include <telebot/cli/cli.h>
-#include <imgui.h>
+#include <telebot/utils/stun.h>
 
 namespace rccar {
 
 namespace log = telebot::utils::logging;
+namespace stun = telebot::utils::stun;
 
 static telebot::plugins::Plugin* self;
 static bool main_window_open = false;
+static stun::Client* client;
+static std::vector<std::string> list;
+
+struct ClientListener : public stun::ClientListener {
+    void onList(stun::Client* client, const std::vector<std::string>& list) {
+        for (const auto& item : list) {
+            log::info("{}", item);
+        }
+    }
+};
+
+static std::shared_ptr<ClientListener> client_listener = std::make_shared<ClientListener>();
 
 static void imgui_plugin(const telebot::plugins::Plugin& plugin) {
     if (plugin.getId() != self->getId()) {
@@ -30,6 +47,44 @@ static void post_imgui_build() {
 
     ImGui::Begin(self->getName().c_str());
 
+    if (ImGui::CollapsingHeader("Stun Client")) {
+        if (client == nullptr) {
+            static char _ip[16] = "127.0.0.1";
+            static int port = 3478;
+            static char _name[32] = "RCCarClient";
+
+            ImGui::InputText("IP", _ip, 16);
+            ImGui::InputInt("Port", &port);
+            ImGui::InputText("Name", _name, 32);
+
+            if (ImGui::Button("Connect")) {
+                std::thread t([]() {
+                    log::info("Connecting to STUN server...");
+
+                    /*std::string ip = _ip;
+                    std::string name = _name;
+
+                    client = new stun::Client(client_listener, "musca");
+                    client->connect("127.0.0.1", 3478);*/
+                });
+            }
+        }
+
+        if (client != nullptr) {
+            if (ImGui::Button("Disconnect")) {
+                client->disconnect();
+                //delete client;
+                //client = nullptr;
+            }
+
+            ImGui::SeparatorText("List");
+
+            /*for (const auto& item : list) {
+                ImGui::Text("%s", item.c_str());
+            }*/
+        }
+    }
+
     ImGui::End();
 }
 
@@ -40,6 +95,18 @@ void main(const telebot::plugins::Plugin& plugin) {
     telebot::events::post_imgui_build.connect(&post_imgui_build);
 
     log::info("Loaded {}!", plugin.getName());
+
+    /*std::thread t = std::thread([]() {
+        log::info("Connecting to STUN server...");
+    });*/
+
+    std::thread t = std::thread([](void* ptr) {
+        log::info("Connecting to STUN server...");
+
+        return 0;
+    });
+
+    std::this_thread::sleep_for(std::chrono::seconds(5));
 }
 
 } // namespace rccar
