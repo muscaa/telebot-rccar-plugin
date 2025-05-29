90f6ab9229df2e29906c5dee08c8462e0f2b6708
Musca Mihail <muscamihaill@gmail.com>
2025-05-29 13:31:46 +0300

    added basic help

--- plugin/src/rccar/cli/cli.cpp
+++ plugin/src/rccar/cli/cli.cpp
@@ -1,14 +1,28 @@
 #include "rccar/cli/cli.h"
 
+#include <string>
+#include <vector>
 #include <telebot/utils/logging.h>
 #include <telebot/cli/cli.h>
+#include <telebot/utils/stun.h>
 
 namespace rccar::cli {
 
 namespace log = telebot::utils::logging;
+namespace stun = telebot::utils::stun;
 
 int main_cli(const telebot::plugins::Plugin& plugin) {
-    log::info("Plugin {} main CLI called with {} args", plugin.getId(), telebot::cli::args.size());
+    const std::vector<std::string>& args = telebot::cli::args;
+
+    if (args.size() <= 2 || args[2] == "--help") {
+        log::info("Usage: {} {} <--help|--stun-server>", args[0], args[1]);
+        return 1;
+    }
+
+    if (args[2] == "--stun-server") {
+
+    }
+
     return 0;
 }
 
