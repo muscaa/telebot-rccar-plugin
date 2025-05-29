2b97103bf0f2a0998aa9f2c4d4ad62f942114486
Musca Mihail <muscamihaill@gmail.com>
2025-05-29 13:10:10 +0300

    update name & plugin json

--- scripts/config.py
+++ scripts/config.py
@@ -1,7 +1,7 @@
 from utils.package import *
 from utils import platform as p
 
-NAME: str = "telebot-template-plugin"
+NAME: str = "telebot-rccar-plugin"
 EXECUTABLE: Executable = Executable("telebot_sdk/telebot")
 
 PACKAGE_LIBRARIES: list[Lib] = [
@@ -12,10 +12,10 @@ PACKAGE_OTHER: list[File] = [
 ]
 PLUGIN_JSON: dict = {
     "id": NAME,
-    "name": NAME,
+    "name": "RC Car Plugin",
     "author": "muscaa",
     "version": "1.0.0",
-    "description": "A template plugin for telebot",
+    "description": "A telebot plugin for controlling an RC car.",
     "plugin_lib": NAME,
     "plugin_main": "ttp::main",
     "plugin_main_cli": "ttp::main_cli",
